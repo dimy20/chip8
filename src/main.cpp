a7af25a1abaa2b4f5a64128c9d7c4e372742909a
dimy20 <enzostrindade16@outlook.com>
2022-09-21 19:03:48 -0300

    main

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,35 @@
 #include <iostream>
-#include "chip8.h"
+#include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <unistd.h>
+#include <sys/time.h>
 
+#include "chip8.h"
+#include "Shader.h"
+#include "Screen.h"
 
 int main(){
-	Chip8 chip;
-	std::cout << "hello world"<< std::endl;
 
+	std::shared_ptr<Chip8> chip = std::make_shared<Chip8>();
+	chip->load_program(nullptr);
+	std::shared_ptr<Screen> screen = std::make_shared<Screen>(chip);
 
-	chip.init();
-	chip.load_program(nullptr);
+	while(!glfwWindowShouldClose(screen->window())){
+		screen->clear();
+		if(glfwGetKey(screen->window(), GLFW_KEY_ESCAPE) == GLFW_PRESS){
+			glfwSetWindowShouldClose(screen->window(), true);
+		}
 
-	chip.emulate_cycle();
-	chip.emulate_cycle();
-	//exit(0);
+		auto chip_elapsed_time = chip->emulate_cycles();
+		auto render_elapsed_time = screen->render();
+		long delta = (chip_elapsed_time - render_elapsed_time) / 1000;
+		if(delta > 0) usleep(delta);
+
+		int fps = screen->fps();
+		//if(fps > 0) std::cout << "FPS : " << fps << std::endl;
+		screen->swap_buffers();
+	 };
+	 screen->close();
+
 	return 0;
 }
